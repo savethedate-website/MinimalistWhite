12098f86d922245816ed099233a344c656edb83a
kson17 <carlsonlazorus@gmail.com>
2024-04-13 21:30:06 +0800

    Implemented display modal when buttons in bottom nav clicked

--- script.js
+++ script.js
@@ -114,3 +114,88 @@ navItems.forEach(function (e, i) {
   });
 });
 
+// Get the modal elements
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+
+// Get the buttons that open the modals
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+
+// Get the <span> elements that close the modals
+var close1 = document.getElementById("close1");
+var close2 = document.getElementById("close2");
+var close3 = document.getElementById("close3");
+var close4 = document.getElementById("close4");
+
+// Function to open modal 1
+btn1.onclick = function () {
+  modal1.style.display = "block";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+  modal4.style.display = "none";
+}
+
+// Function to close modal 1
+close1.onclick = function () {
+  modal1.style.display = "none";
+}
+
+// Function to open modal 2
+btn2.onclick = function () {
+  modal1.style.display = "none";
+  modal2.style.display = "block";
+  modal3.style.display = "none";
+  modal4.style.display = "none";
+}
+
+// Function to close modal 2
+close2.onclick = function () {
+  modal2.style.display = "none";
+}
+
+// Function to open modal 3
+btn3.onclick = function () {
+  modal1.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "block";
+  modal4.style.display = "none";
+}
+
+// Function to close modal 3
+close3.onclick = function () {
+  modal3.style.display = "none";
+}
+
+// Function to open modal 4
+btn4.onclick = function () {
+  modal1.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+  modal4.style.display = "block";
+}
+
+// Function to close modal 4
+close4.onclick = function () {
+  modal4.style.display = "none";
+}
+
+// Close modals when clicking outside modal content
+window.onclick = function (event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
